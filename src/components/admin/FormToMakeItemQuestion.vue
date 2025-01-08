e67bc6da589eb587337a260af57228b27ca26713
KangSDhi <kangteknisi@gmail.com>
2025-01-08 14:34:50 +0700

    feat(admin): onprogress FormToMakeItemQuestion.vue

--- src/components/admin/FormToMakeItemQuestion.vue
+++ src/components/admin/FormToMakeItemQuestion.vue
@@ -1,21 +1,63 @@
 <template>
-    <div class="container mx-auto p-10 bg-slate-500 rounded-md shadow-lg">
-        <Editor 
-            :api-key="tinyMCEApiKey"
-            :init="editorConfig"/>
-        <ul>
-            <li class="flex flex-row gap-2">
-                <div>
-                    A.
-                </div>
-                <input type="text">
+    <div class="container mx-auto py-5 px-2 bg-gray-200 rounded-md shadow-lg">
+        <h1 class="text-xl mb-2">Tambah Soal</h1>
+        <div class="flex flex-row gap-2">
+            <Editor :api-key="tinyMCEApiKey" :init="editorConfig" />
+            <div class="flex flex-col">
+                Gambar Pertanyaan
+                <img src="/img/image.svg" class="w-48 h-48" alt="Gambar Pertanyaan Preview" srcset="">
+                <input type="file">
+            </div>
+        </div>
+        <ul class="flex flex-col m-3">
+            <li class="flex flex-row gap-2 mb-2">
+                <div class="w-8 h-8 text-base text-center">A.</div>
+                <input type="text"
+                    class="w-1/4 py-1 px-1 rounded-md bg-slate-200 border border-black focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500">
                 <input type="file">
             </li>
-            <li>
-                B.
-                <input type="text">
+            <li class="flex flex-row gap-2 mb-2">
+                <div class="w-8 h-8 text-base text-center">B.</div>
+                <input type="text"
+                    class="w-1/4 py-1 px-1 rounded-md bg-slate-200 border border-black focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500">
+                <input type="file">
+            </li>
+            <li class="flex flex-row gap-2 mb-2">
+                <div class="w-8 h-8 text-base text-center">C.</div>
+                <input type="text"
+                    class="w-1/4 py-1 px-1 rounded-md bg-slate-200 border border-black focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500">
+                <input type="file">
+            </li>
+            <li class="flex flex-row gap-2 mb-2">
+                <div class="w-8 h-8 text-base text-center">D.</div>
+                <input type="text"
+                    class="w-1/4 py-1 px-1 rounded-md bg-slate-200 border border-black focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500">
+                <input type="file">
+            </li>
+            <li class="flex flex-row gap-2 mb-2">
+                <div class="w-8 h-8 text-base text-center">E.</div>
+                <input type="text"
+                    class="w-1/4 py-1 px-1 rounded-md bg-slate-200 border border-black focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500">
+                <input type="file">
             </li>
         </ul>
+        <div class="flex flex-row gap-2 px-5">
+            <button class="flex py-1.5 px-1 bg-blue-400 rounded-md text-base">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                </svg>
+                Simpan
+            </button>
+            <button class="flex py-1.5 px-1 bg-red-500 rounded-md">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                </svg>
+                Hapus
+            </button>
+        </div>
     </div>
 </template>
 
@@ -36,6 +78,8 @@ export default {
             tinyMCEApiKey: import.meta.env.VITE_TINY_MCE_API_KEY,
             editorConfig: {
                 toolbar_mode: 'sliding',
+                width: 800,
+                height: 250,
                 plugins: [
                     // Core editing features
                     'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'link', 'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount',
--- src/components/admin/QuestionsTable.vue
+++ src/components/admin/QuestionsTable.vue
@@ -37,7 +37,7 @@
         </div>
     </div>
     <div class="relative overflow-x-auto">
-        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <table class="w-full table-auto text-sm text-left text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
                     <th scope="col" class="px-6 py-3">
@@ -207,16 +207,17 @@
                 <template v-for="(item, index) in questionsDataTemp" :key="index">
                     <tr v-show="checkView(index + 1)"
                         class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        <th scope="row"
+                            class="px-6 py-4 font-medium text-xs text-gray-900 whitespace-nowrap dark:text-white">
                             {{ index + 1 }}
                         </th>
-                        <td class="px-6 py-4">
+                        <td class="px-4 py-2 text-xs">
                             {{ item.nama_soal }}
                         </td>
-                        <td class="px-6 py-4">
+                        <td class="px-4 py-2 text-xs text-center">
                             {{ item.tingkat }}
                         </td>
-                        <td class="px-6 py-4">
+                        <td class="px-4 py-2 text-xs text-center">
                             <template v-if="item.jurusan == null">
                                 Semua Jurusan
                             </template>
@@ -224,52 +225,51 @@
                                 {{ item.jurusan }}
                             </template>
                         </td>
-                        <td class="px-6 py-4">
+                        <td class="px-4 py-2 text-xs text-center">
                             {{ item.butir_soal }}
                         </td>
-                        <td class="px-6 py-4">
+                        <td class="px-4 py-2 text-xs text-center">
                             {{ item.durasi_soal }}
                         </td>
-                        <td class="px-6 py-4">
+                        <td class="px-2 py-1 text-xs">
                             {{ item.waktu_mulai_soal }}
                         </td>
-                        <td class="px-6 py-4">
+                        <td class="px-2 py-1 text-xs">
                             {{ item.waktu_selesai_soal }}
                         </td>
-                        <td class="px-6 py-4 space-x-2 text-right">
-                            <button class="bg-sky-200 px-2.5 py-1 text-gray-900 rounded-md inline-flex items-center">
+                        <td class="grid grid-cols-4 px-4 py-2 gap-1">
+                            <button class="bg-sky-200 p-1 text-gray-900 rounded-md inline-flex items-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                     stroke-width="1.5" stroke="currentColor" class="w-4 -h-4">
                                     <path stroke-linecap="round" stroke-linejoin="round"
                                         d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75" />
                                 </svg>
-                                Salin
                             </button>
-                            <RouterLink :to="{ name: 'Question Management Item Admin Page', params:{ idSoal: encryption(item.id) } }" target="_blank" class="bg-yellow-200 px-2.5 py-1 text-gray-900 rounded-md inline-flex items-center">
+                            <RouterLink
+                                :to="{ name: 'Question Management Item Admin Page', params: { idSoal: encryption(item.id) } }"
+                                target="_blank"
+                                class="bg-yellow-200 p-1 text-gray-900 rounded-md inline-flex items-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                     stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                                     <path stroke-linecap="round" stroke-linejoin="round"
                                         d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
                                 </svg>
-                                Detail
                             </RouterLink>
                             <button @click="formToEditQuestionOpen(item.id)"
-                                class="bg-blue-200 px-2.5 py-1 text-gray-900 rounded-md inline-flex items-center">
+                                class="bg-blue-200 p-1 text-gray-900 rounded-md inline-flex items-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                     stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                                     <path stroke-linecap="round" stroke-linejoin="round"
                                         d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                                 </svg>
-                                Edit
                             </button>
                             <button @click="dialogToDeleteQuestionOpen(item.id)"
-                                class="px-2.5 py-1 text-red-500 rounded-md inline-flex items-center">
+                                class="p-1 text-red-500 rounded-md inline-flex items-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                     stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                                     <path stroke-linecap="round" stroke-linejoin="round"
                                         d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                 </svg>
-                                Hapus
                             </button>
                         </td>
                     </tr>
@@ -338,7 +338,7 @@ export default {
         "isFormToEditQuestionTrue",
         "isDialogToDeleteQuestionTrue"
     ],
-    setup(){
+    setup() {
         return {
             encryption,
         }
